e2703bd9ec958d25a1be622c3daf4449ed23e2ce
Julia Kimmig <juliakimmig@gmail.com>
2019-03-27 11:57:13 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "primavera", address: "shanghai", category: "italian" )
+restaurant2 = Restaurant.create!(name: "shintori", address: "shanghai", category: "japanese" )
+restaurant3 = Restaurant.create!(name: "mr&mrsbund", address: "shanghai", category: "french" )
+restaurant4 = Restaurant.create!(name: "musselhouse", address: "shanghai", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "ultraviolet", address: "shanghai", category: "chinese" )
